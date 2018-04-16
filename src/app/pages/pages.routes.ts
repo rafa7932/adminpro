f1b245c62f31a77257f3d4229a6d2a17d96a4414
Rafael Hinojosa <rafaelhrr@gmail.com>
2018-04-15 20:15:33 -0500

    Input, output y viewchild

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,24 @@
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const pagesRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+       children: [
+           { path: 'dashboard', component: DashboardComponent},
+           { path: 'progress', component: ProgressComponent},
+           { path: 'graficas1', component: Graficas1Component},
+           { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+       ]
+   }
+];
+
+
+export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes );
